b46c2d75565f2588917226e5898e0315c265e7bf
Atomik <atom1k.ex@gmail.com>
2021-05-20 06:15:12 -0700

    Created Menu List for homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DirComp from '../../components/directory/directory'
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+<DirComp/>
+  </div>
+);
+
+export default HomePage;
